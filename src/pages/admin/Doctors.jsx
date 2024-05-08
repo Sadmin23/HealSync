f15115d04ed3d9e2ff37c7728696c95c11be1c86
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2024-05-08 19:05:54 +0600

    add emergency and doctor table

--- src/pages/admin/Doctors.jsx
+++ src/pages/admin/Doctors.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
-import CareTeam from '../../components/CareTeam'
-import Prescriptions from '../../components/Prescriptions'
-import Reports from '../../components/Report'
+import DoctorList from './DoctorList'
 
 export default function Doctors() {
   return (
-    <div className='h-screen'>
-      <div className='w-[1200px] flex justify-between items-center mt-8 ml-10 rounded-lg h-72'>
-        <CareTeam/>
-        <Prescriptions update={true}/>
-        <Reports update={true}/>
-      </div>
+    <div className='h-screen ml-8 mt-8'>
+      <h1 className="text-lg font-bold text-green-900 mx-2">EMERGENCIES</h1>
+      <header className="flex mt-6 text-white bg-green-900 font-semibold border border-green-950 rounded-t-md">
+        <div className="pl-6 py-2 w-52">Doctor ID</div>
+        <div className="pl-6 py-2 w-48">Doctor Name</div>
+        <div className='pl-6 py-2 w-32'>Gender</div>
+        <div className="pl-6 py-2 w-48">Specalization</div>
+        <div className="pl-6 py-2 w-48">Phone</div>
+        <div className="pl-6 py-2 w-60">Email</div>
+      </header>
+      <body>
+        {
+          DoctorList.map((doctor, index) => (
+            <div key={index} className={`flex font-semibold border-x border-green-950 ${index % 2 === 0 ? 'bg-green-50' : 'bg-green-200'} ${index === (DoctorList.length - 1) ?  `border-b rounded-b` : ``}`}>
+              <div className="pl-6 py-2 w-52">{doctor.doctorId}</div>
+              <div className="pl-6 py-2 w-48">{doctor.name}</div>
+              <div className="pl-6 py-2 w-32">{doctor.gender}</div>
+              <div className="pl-6 py-2 w-48">{doctor.specializations}</div>
+              <div className="pl-6 py-2 w-48">{doctor.phone}</div>
+              <div className="pl-6 py-2 w-60">{doctor.email}</div>
+            </div>
+          ))
+        }       
+      </body>
     </div>
   )
 }
--- src/pages/admin/Emergencies.jsx
+++ src/pages/admin/Emergencies.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import AllEmergency from './AllEmergencies'
 
 export default function Emergencies() {
   return (
-    <div className='h-screen'>Emergencies</div>
+    <div className='h-screen p-8'>
+      <h1 className="text-lg font-bold text-green-900 mx-2">EMERGENCIES</h1>
+      <header className="flex mt-6 text-white bg-green-900 font-semibold border border-green-950 rounded-t-md">
+        <div className="pl-6 py-2 w-44">Patient ID</div>
+        <div className="pl-6 py-2 w-40">Patient Name</div>
+        <div className='pl-6 py-2 w-44'>Doctor ID</div>
+        <div className="pl-6 py-2 w-40">Doctor Name</div>
+        <div className="pl-6 py-2 w-60">Emergency Time</div>
+        <div className="pl-6 py-2 w-60">Action</div>
+      </header>
+      <body>
+        {
+          AllEmergency.map((emergency, index) => (
+            <div key={index} className={`flex font-semibold border-x border-green-950 ${index % 2 === 0 ? 'bg-green-50' : 'bg-green-200'} ${index === (AllEmergency.length - 1) ?  `border-b rounded-b` : ``}`}>
+              <div className="pl-6 py-2 w-44">{emergency.doctorId}</div>
+              <div className="pl-6 py-2 w-40">{emergency.doctorName}</div>
+              <div className="pl-6 py-2 w-44">{emergency.patientId}</div>
+              <div className="pl-6 py-2 w-40">{emergency.patientName}</div>
+              <div className="pl-6 py-2 w-60">{emergency.time}</div>
+              <div className='flex items-center ml-6 w-60'>{emergency.action}</div>
+            </div>
+          ))
+        }       
+      </body>
+    </div>
   )
 }
