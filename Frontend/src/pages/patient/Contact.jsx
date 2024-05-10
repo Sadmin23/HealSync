ac3ab92e069633f099201010b742594b4b9ffd8f
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2024-05-10 17:58:23 +0600

    add video calling feature

--- Frontend/src/pages/patient/Contact.jsx
+++ Frontend/src/pages/patient/Contact.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 export default function Contact() {
+
+  const user = useSelector((state) => state.user.currentUser);
+  const name = user.username;
+
+  const handleEmergencyCall = async () => {
+    try {
+      const currentTime = new Date().toLocaleTimeString();
+
+      const response = await fetch('http://localhost:8000/emergencies', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: name,
+          time: currentTime,
+          action: 'false'
+        })
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to make emergency call');
+      }
+
+      window.open('http://localhost:3001/', '_blank');
+    } catch (error) {
+      console.error('Error making emergency call:', error);
+    }
+  };
+
   return (
-    <div className='h-screen'>Contact</div>
-  )
+    <div className='h-screen flex ml-96 mt-40'>
+      <button
+        className='bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-4 rounded h-10'
+        onClick={handleEmergencyCall} // Attach the event handler to the onClick event
+      >
+        Emergency Call
+      </button>
+    </div>
+  );
 }
--- Frontend/src/shared/Layout.jsx
+++ Frontend/src/shared/Layout.jsx
@@ -1,16 +1,110 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
 import Sidebar from './Sidebar'
+import { useSelector } from 'react-redux';
 
 export default function Layout() {
+
+  const user = useSelector((state) => state.user.currentUser);
+  const userType = user.user;
+
+  const [emergencies, setEmergencies] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [responded, setResponded] = useState(false);
+  const [caller, setCaller] = useState('');
+
+  const calculateTimeDifference = (time1, time2) => {
+    const parsedTime1 = parseTimeString(time1);
+    const parsedTime2 = parseTimeString(time2);
+  
+    const totalSeconds1 = parsedTime1.hours * 3600 + parsedTime1.minutes * 60 + parsedTime1.seconds;
+    const totalSeconds2 = parsedTime2.hours * 3600 + parsedTime2.minutes * 60 + parsedTime2.seconds;
+  
+    const differenceInSeconds = Math.abs(totalSeconds2 - totalSeconds1);
+    return differenceInSeconds;
+  };
+  
+  const parseTimeString = (timeString) => {
+    const [time, meridiem] = timeString.split(' ');
+    const [hours, minutes, seconds] = time.split(':').map(Number);
+  
+    const parsedHours = meridiem === 'PM' ? hours + 12 : hours;
+  
+    return {
+      hours: parsedHours,
+      minutes: minutes,
+      seconds: seconds
+    };
+  };
+
+  const fetchEmergencies = async () => {
+    const response = await fetch('http://localhost:8000/emergencies');
+    const data = await response.json();
+    setEmergencies(data);
+    if (data.length > 0 && userType === 'Doctor') {
+      let callTime = data[data.length - 1].time;
+      let currentTime = new Date().toLocaleTimeString();
+      const timeDifference = calculateTimeDifference(callTime, currentTime);
+      timeDifference < 30 && data[data.length-1].action === 'false' ? setShowForm(true) : setShowForm(false);
+    }
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(fetchEmergencies, 3000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+
+  useEffect(() => {
+    setCaller(emergencies[emergencies.length - 1]?.name);
+  }, [emergencies])
+
+  const handleEmergencyCall = async () => {
+    try {
+      window.open('http://localhost:3001/?roomID=123456', '_blank');
+  
+      const updateData = { action: 'Emergency call responded' };
+  
+      const response = await fetch('http://localhost:8000/emergencies/', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updateData),
+      });
+  
+      if (!response.ok) {
+        throw new Error('Failed to update emergency record');
+      }
+  
+      setShowForm(false);
+    } catch (error) {
+      console.error('Error updating emergency record:', error);
+    }
+  };
+
   return (
     <div className='flex-col bg-slate-200'>
-          <Header/>
+        <Header/>
         <div className='flex'>
           <Sidebar/>
           <div>{<Outlet/>}</div>
         </div>
+        {showForm && (
+          <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-50">
+            <div className="bg-white px-12 pt-8 pb-8 rounded-lg">
+              <h1 className='text-xl font-semibold text-gray-700 mb-8'>{caller} is making an emergency call</h1>
+              <div className='bg-white'>
+                <div className="flex justify-center">
+                  <button type="submit" className="bg-red-600 hover:bg-red-900 text-white text-lg font-semibold py-4 px-6 rounded-md hover:bg-primary-600" onClick={handleEmergencyCall}>
+                    Respond
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
     </div>
   )
 }
