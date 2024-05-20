7b176ebef638fa6faac7c848c0e5d6da79f8cf3e
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2024-05-20 18:17:53 +0600

    Remove backend folder

--- Backend/models/nurse.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field
-
-class Nurse(BaseModel):
-    nurse_id: str = Field(..., alias="nurseId")
-    name: str
-    password: str
-    gender: str
-    phone: str
-    email: str
