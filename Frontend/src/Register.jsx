e49fb08670489c04819e6ac79ba7f8ca324a2425
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2024-06-14 16:59:38 +0600

    update login register UI

--- Frontend/src/Register.jsx
+++ Frontend/src/Register.jsx
@@ -3,6 +3,7 @@ import DoctorForm from './components/DoctorForm';
 import NurseForm from './components/NurseForm';
 import PatientForm from './components/PatientForm';
 import { Link } from 'react-router-dom';
+import main from './assets/main.png';
 
 export default function Register() {
     const [user, setUser] = useState('patient');
@@ -13,34 +14,40 @@ export default function Register() {
 
 
     return (
-        <div className='max-w-xl mx-auto'>
-            <h1 className='text-3xl text-center font-bold my-4'>Register</h1>
-            <div className='flex space-x-10 flex-wrap justify-center mb-4'>
-                <button 
-                    className={`${user === 'patient' ? `bg-blue-700 shadow-2xl` : 'bg-blue-400'}  hover:shadow-md text-white font-bold py-2 px-4 rounded`}
-                    onClick={() => handleUserChange('patient')}
-                >
-                    Patient
-                </button>
-                <button 
-                    className={`${user === 'doctor' ? `bg-pink-700 shadow-2xl` : 'bg-pink-400'}  hover:shadow-md text-white font-bold py-2 px-4 rounded`}
-                    onClick={() => handleUserChange('doctor')}
-                >
-                    Doctor
-                </button>
-                <button 
-                    className={`${user === 'nurse' ? `bg-orange-700 shadow-2xl` : 'bg-orange-400'}  hover:shadow-md text-white font-bold py-2 px-4 rounded`}
-                    onClick={() => handleUserChange('nurse')}
-                >
-                    Nurse
-                </button>
+        <div className='flex'>
+            <div className='w-1/2 bg-slate-200 h-screen'>
+              <h1 className='mt-20 text-center text-5xl font-mono font-bold'>Welcome to HealSync</h1>
+              <img src={main} alt='main' className='mx-auto mt-10 w-[550px] h-96 object-cover'/>
             </div>
-            {user === 'doctor' ? <DoctorForm /> : user === 'nurse' ? <NurseForm/> : <PatientForm/>}
-            <div className='flex gap-2 mt-5 mb-10'>
-              <p>Already have an account?</p>
-              <Link to={'/'}>
-                <span className='text-blue-700'>Sign In</span>
-              </Link>
+            <div className='w-2/5 mx-auto'>
+                <h1 className='text-3xl text-center font-bold my-4'>Register</h1>
+                <div className='flex space-x-10 flex-wrap justify-center mb-4'>
+                    <button 
+                        className={`${user === 'patient' ? `bg-blue-700 shadow-2xl` : 'bg-blue-400'}  hover:shadow-md text-white font-bold py-2 px-4 rounded`}
+                        onClick={() => handleUserChange('patient')}
+                    >
+                        Patient
+                    </button>
+                    <button 
+                        className={`${user === 'doctor' ? `bg-pink-700 shadow-2xl` : 'bg-pink-400'}  hover:shadow-md text-white font-bold py-2 px-4 rounded`}
+                        onClick={() => handleUserChange('doctor')}
+                    >
+                        Doctor
+                    </button>
+                    <button 
+                        className={`${user === 'nurse' ? `bg-orange-700 shadow-2xl` : 'bg-orange-400'}  hover:shadow-md text-white font-bold py-2 px-4 rounded`}
+                        onClick={() => handleUserChange('nurse')}
+                    >
+                        Nurse
+                    </button>
+                </div>
+                {user === 'doctor' ? <DoctorForm /> : user === 'nurse' ? <NurseForm/> : <PatientForm/>}
+                <div className='flex gap-2 mt-5 mb-10'>
+                  <p>Already have an account?</p>
+                  <Link to={'/'}>
+                    <span className='text-blue-700'>Sign In</span>
+                  </Link>
+                </div>
             </div>
         </div>
     );
--- Frontend/src/SignIn.jsx
+++ Frontend/src/SignIn.jsx
@@ -2,6 +2,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux'
 import { SignInUser } from './redux/userSlice'
 import { useEffect, useState } from 'react';
+import main from './assets/main.png'
 
 export default function SignIn() {
 
@@ -91,54 +92,60 @@ export default function SignIn() {
   }
 
   return (
-    <div onSubmit={handleSubmit} className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-bold my-7'>Sign In</h1>
-      <form className='flex flex-col gap-4'>
-        <select
+    <div className='flex'>
+      <div className='w-1/2 bg-slate-200 h-screen'>
+        <h1 className='mt-20 text-center text-5xl font-mono font-bold'>Welcome to HealSync</h1>
+        <img src={main} alt='main' className='mx-auto mt-10 w-[550px] h-96 object-cover'/>
+      </div>
+      <div onSubmit={handleSubmit} className='p-3 mt-20 w-2/5 mx-auto'>
+        <h1 className='text-4xl tracking-wide text-center font-bold my-7'>Sign In</h1>
+        <form className='flex flex-col gap-4'>
+          <select
+              className='border p-3 rounded-lg'
+              id="user"
+              value={formState.user}
+              onChange={handleChange}
+              name="user"
+              required
+            >
+              <option value="None" disabled>Choose a user</option>
+              <option value="Admin">Admin</option>
+              <option value="Doctor">Doctor</option>
+              <option value="Nurse">Nurse</option>
+              <option value="Patient">Patient</option>
+          </select>
+          <input
+            type='text'
+            placeholder='Username'
             className='border p-3 rounded-lg'
-            id="user"
-            value={formState.user}
+            id='username'
+            name='username'
+            value={formState.username}
             onChange={handleChange}
-            name="user"
             required
+          />
+          <input
+            type='password'
+            placeholder='Password'
+            className='border p-3 rounded-lg'
+            id='password'
+            name='password'
+            value={formState.password}
+            onChange={handleChange}
+            required
+          />
+          <button type='submit' className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
           >
-            <option value="None" disabled>Choose a user</option>
-            <option value="Admin">Admin</option>
-            <option value="Doctor">Doctor</option>
-            <option value="Nurse">Nurse</option>
-            <option value="Patient">Patient</option>
-        </select>
-        <input
-          type='text'
-          placeholder='Username'
-          className='border p-3 rounded-lg'
-          id='username'
-          name='username'
-          value={formState.username}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type='password'
-          placeholder='Password'
-          className='border p-3 rounded-lg'
-          id='password'
-          name='password'
-          value={formState.password}
-          onChange={handleChange}
-          required
-        />
-        <button type='submit' className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
-        >
-          Sign In
-        </button>
-        {error && <div className="text-red-500 mt-2">{error}</div>}
-      </form>
-      <div className='flex gap-2 mt-5'>
-        <p>Don't have an account?</p>
-        <Link to={'/register'}>
-          <span className='text-blue-700'>Sign Up</span>
-        </Link>
+            Sign In
+          </button>
+          {error && <div className="text-red-500 mt-2">{error}</div>}
+        </form>
+        <div className='flex gap-2 mt-5'>
+          <p>Don't have an account?</p>
+          <Link to={'/register'}>
+            <span className='text-blue-700'>Sign Up</span>
+          </Link>
+        </div>
       </div>
     </div>
   );
